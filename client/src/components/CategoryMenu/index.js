a400dabf754a8ae79f4ae6bc5a7ea63efc038cca
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-01 17:38:05 -0800

    updated categorymenu component

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+//22.1.6 added to help pull informaton from an async function
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+//22.1.6 - to be used on dispatch method
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+//22.1.6
+import { useStoreContext } from '../../utils/GlobalState';
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
+  //22.1.6
+  const [state, dispatch] = useStoreContext();
+  //22.1.6
+  const { categories } = state;
+  //22.1.6
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+  useEffect(() => {
+    //if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories,
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  const handleClick = (id) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+  };
 
   return (
     <div>
@@ -13,7 +43,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
