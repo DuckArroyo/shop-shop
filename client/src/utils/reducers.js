6321bb6352ab8d56a1194071f6fa34d8bc80e725
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-01 16:15:25 -0800

    added custom react hook

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,6 +4,8 @@ import {
   UPDATE_CURRENT_CATEGORY,
 } from './actions';
 
+import { useReducer } from 'react';
+
 export const reducer = (state, action) => {
   switch (action.type) {
     // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
@@ -12,9 +14,24 @@ export const reducer = (state, action) => {
         ...state,
         products: [...action.products],
       };
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
 
-    // if it's none of these actions, do not update state at all and keep things the same!
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
     default:
       return state;
   }
 };
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
